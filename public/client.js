b4a1184ca636a15302125a88b8231b6b0f65202f
SpyrosSiannas <ssiannas@gmail.com>
2024-11-17 16:27:43 +0200

    Add snowflakes and audio

--- public/client.js
+++ public/client.js
@@ -1,3 +1,18 @@
+ //===================== audio
+  var source = "music/music.mp3"
+  var audio = document.createElement("audio");
+  audio.autoplay = true;
+  audio.controls = true;
+  audio.volume = 0.01; // 0.1
+  //
+  audio.load()
+  audio.addEventListener("load", function() { 
+      audio.play(); 
+  }, true);
+  audio.src = source;
+  document.body.appendChild(audio);
+
+
 document.addEventListener('DOMContentLoaded', async () => {
   const nameInput = document.getElementById('nameInput');
   const joinButton = document.getElementById('joinButton');
@@ -106,4 +121,76 @@ document.addEventListener('DOMContentLoaded', async () => {
       status.textContent = msg;
     }
   };
+
+  //===================== snow
+  const NUMBER_OF_SNOWFLAKES = 300;
+  const MAX_SNOWFLAKE_SIZE = 5;
+  const MAX_SNOWFLAKE_SPEED = 2;
+  const SNOWFLAKE_COLOUR = '#ddddddb3';
+  const snowflakes = [];
+
+  const canvas = document.createElement('canvas');
+  canvas.style.position = 'absolute';
+  canvas.style.pointerEvents = 'none';
+  canvas.style.top = '0px';
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  document.body.appendChild(canvas);
+
+  const ctx = canvas.getContext('2d');
+
+
+  const createSnowflake = () => ({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.floor(Math.random() * MAX_SNOWFLAKE_SIZE) + 1,
+      color: SNOWFLAKE_COLOUR,
+      speed: Math.random() * MAX_SNOWFLAKE_SPEED + 1,
+      sway: Math.random() - 0.5 // next
+  });
+
+  const drawSnowflake = snowflake => {
+      ctx.beginPath();
+      ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2);
+      ctx.fillStyle = snowflake.color;
+      ctx.fill();
+      ctx.closePath();
+  }
+
+  const updateSnowflake = snowflake => {
+      snowflake.y += snowflake.speed;
+      snowflake.x += snowflake.sway; // next
+      if (snowflake.y > canvas.height) {
+          Object.assign(snowflake, createSnowflake());
+      }
+  }
+
+  const animate = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      snowflakes.forEach(snowflake => {
+          updateSnowflake(snowflake);
+          drawSnowflake(snowflake);
+      });
+
+      requestAnimationFrame(animate);
+  }
+
+  for (let i = 0; i < NUMBER_OF_SNOWFLAKES; i++) {
+      snowflakes.push(createSnowflake());
+  }
+
+  window.addEventListener('resize', () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+  });
+
+  window.addEventListener('scroll', () => {
+      canvas.style.top = `${window.scrollY}px`;
+  });
+
+  // setInterval(animate, 15);
+  animate()
+
+
 });
